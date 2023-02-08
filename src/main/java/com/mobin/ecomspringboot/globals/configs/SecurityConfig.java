7ab1e5476e982dbdaf4a5e9cd6f0cef5dcc19350
mobin-newroz <ahmobin@newroztech.com>
2023-02-08 11:00:24 +0600

    feat : security dependency add

---
+++ src/main/java/com/mobin/ecomspringboot/globals/configs/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.mobin.ecomspringboot.globals.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+//        httpSecurity
+//                .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic();
+//
+//        return httpSecurity.build();
+//    }
+}
---
+++ src/main/java/com/mobin/ecomspringboot/v1/inventory/models/response/ProductResponseModel.java
@@ -0,0 +1,12 @@
+package com.mobin.ecomspringboot.v1.inventory.models.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProductResponseModel {
+    private String name;
+}
